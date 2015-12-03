8e743815d92cbe4436761ff78d516aefad03ab9b
Monty Lennie <montylennie@gmail.com>
2015-12-03 16:09:41 +0000

    add node_modules to gitignore and start setting up powerball handlers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+      powerball = require('../util/powerball'),
+      response = require('../util/response'),
+      router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.json('index', { title: 'Express' });
 });
+
+/* GET jackpot */
+router.get('/jackpot', function(req, res) {
+  powerball.getJackpot()
+    .then(response.returnSuccess)
+    .catch(response.returnError);
+});
+
+/* GET winning numbers */
+router.get('/numbers', function(req, res) {
+  powerball.getWinningNumbers()
+    .then(response.returnSuccess)
+    .catch(response.returnError);
+});
+
 
 module.exports = router;
