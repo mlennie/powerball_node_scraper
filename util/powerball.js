a2b3bbf582eec946955568320ad685dc6436ac28
Monty Lennie <montylennie@gmail.com>
2015-12-04 04:30:26 +0000

    refactor numbers parsing

--- util/powerball.js
+++ util/powerball.js
@@ -20,9 +20,8 @@ exports.getNumbers = () => {
     return request.get('http://www.powerball.com/powerball/winnums-text.txt');
   })
   .then((res) => {
-    console.log(res.text);
-    var numbers = res.text.split('\n')[1].split('  ').slice(1).join(' ').trim('\r');
-    return numbers.split(' ').map(function(n) {return +n}) ;
+    var numbers = res.text.split('\n')[1];
+    return numbers.split('  ').map(function(n){return +n}).slice(1);
   });
 };
 
